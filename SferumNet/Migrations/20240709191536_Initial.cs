2216c17e82ff17c8ff4d0b0909fbc6c3caee161a
TheCraztWolf <tcrazywolf63@gmail.com>
2024-07-09 23:16:19 +0400

    Added migrations

---
+++ SferumNet/Migrations/20240709191536_Initial.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SferumNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VkProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    RemixSid = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: false),
+                    AccessToken = table.Column<string>(type: "TEXT", nullable: false),
+                    AccessTokenExpired = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VkProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WelcomeSentences",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Message = table.Column<string>(type: "TEXT", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 21, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WelcomeSentences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scenarios",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdProfile = table.Column<long>(type: "INTEGER", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeStart = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    TimeEnd = table.Column<TimeSpan>(type: "TEXT", nullable: false),
+                    IdConversation = table.Column<long>(type: "INTEGER", nullable: false),
+                    LastExecuted = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TotalExecuted = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxToExecute = table.Column<int>(type: "INTEGER", nullable: false),
+                    Delay = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsActiveForWeekend = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    IsShuffleWords = table.Column<bool>(type: "INTEGER", nullable: true),
+                    TypeSchedule = table.Column<int>(type: "INTEGER", nullable: true),
+                    Value = table.Column<string>(type: "TEXT", nullable: true),
+                    IsAddedNextDay = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scenarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Scenarios_VkProfiles_IdProfile",
+                        column: x => x.IdProfile,
+                        principalTable: "VkProfiles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Message = table.Column<string>(type: "TEXT", nullable: false),
+                    IdScenario = table.Column<long>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Logs_Scenarios_IdScenario",
+                        column: x => x.IdScenario,
+                        principalTable: "Scenarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logs_IdScenario",
+                table: "Logs",
+                column: "IdScenario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scenarios_IdProfile",
+                table: "Scenarios",
+                column: "IdProfile");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropTable(
+                name: "WelcomeSentences");
+
+            migrationBuilder.DropTable(
+                name: "Scenarios");
+
+            migrationBuilder.DropTable(
+                name: "VkProfiles");
+        }
+    }
+}
