67db58744b5147cfe10e6931044bdc57bb22e009
TheCraztWolf <tcrazywolf63@gmail.com>
2024-07-10 13:32:57 +0400

    Remove duplicate

--- SferumNet.DbModels/Data/WelcomeSentence.cs
+++ SferumNet.DbModels/Data/WelcomeSentence.cs
@@ -1,9 +1,5 @@
-﻿using SferumNet.DbModels.Common;
-using SferumNet.DbModels.Data.Common;
+﻿using SferumNet.DbModels.Data.Common;
 
 namespace SferumNet.DbModels.Data;
 
-public class WelcomeSentence : BaseMessage
-{
-    public string Message { get; set; } = string.Empty;
-}
+public class WelcomeSentence : BaseMessage;
