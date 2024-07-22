1566f9d74ee410d32bd84cd6966f3b35dbfb836a
TheCrazyWolf <74318176+TheCrazyWolf@users.noreply.github.com>
2024-07-22 22:22:16 +0400

    Delete SferumSharp directory

--- SferumSharp/Program.cs
+++
@@ -1,8 +0,0 @@
-using SferumSharp.Services;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<SferumWorker>();
-builder.Services.AddSingleton<VkFactory>();
-
-var host = builder.Build();
-host.Run();
