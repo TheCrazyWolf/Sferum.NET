475d90b047877e22532c57831325fca26c381907
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-12 20:21:28 +0400

    Исправлено работа с расписанием (новая либа)

--- SferumNet/Scenarios/SchedulesJob.cs
+++ SferumNet/Scenarios/SchedulesJob.cs
@@ -20,31 +20,12 @@ public class SchedulesJob : WelcomesJob
 
         if (upCasted.IsAddedNextDay)
             return await BuilerStringShedule(await _samgk.Sсhedule
-                .GetScheduleAsync(DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(1)), upCasted.TypeSchedule, upCasted.Value));
+                .GetScheduleAsync(DateOnly.FromDateTime(DateTime.Now.AddDays(1)), upCasted.TypeSchedule, upCasted.Value));
         
         return await BuilerStringShedule(await _samgk.Sсhedule
             .GetScheduleAsync(DateOnly.FromDateTime(DateTime.Now), upCasted.TypeSchedule, upCasted.Value));
     }
-
-    private Task<string> BuilerStringShedule(IEnumerable<IResultOutScheduleFromDate>? schedule)
-    {
-        if (schedule is null || schedule.Count() is 0)
-            return Task.FromResult($"Не удалось получить расписание. {new Random().Next()}");
-            
-        var msg = new StringBuilder();
-
-        foreach (var scheduleDate in schedule)
-        {
-            msg.AppendLine($"Расписание на {scheduleDate.Date}");
-            foreach (var lesson in scheduleDate.Lessons)
-            {
-                msg.AppendLine($"# {lesson.NumPair}.{lesson.NumLesson} - {lesson.SubjectDetails.SubjectName} ({lesson.Identity.FirstOrDefault()?.Name}) ауд. № {lesson.Cabs.FirstOrDefault()?.Adress}");
-            }
-        }
-
-        return Task.FromResult(msg.ToString());
-    }
+    
     
     private Task<string> BuilerStringShedule(IResultOutScheduleFromDate? schedule)
     {
@@ -55,11 +36,41 @@ public class SchedulesJob : WelcomesJob
 
         msg.AppendLine($"Расписание на {schedule.Date}");
         
-        foreach (var lesson in schedule.Lessons)
+        foreach (var lesson in schedule.Lessons
+                     .GroupBy(l => new { l.NumPair, l.NumLesson, l.SubjectDetails.SubjectName})
+                     .Select(g => g.First())
+                     .ToList())
         {
-            msg.AppendLine($"# {lesson.NumPair}.{lesson.NumLesson} - {lesson.SubjectDetails.SubjectName} ({lesson.Identity.FirstOrDefault()?.Name}) ауд. № {lesson.Cabs.FirstOrDefault()?.Adress}");
+            msg.AppendLine($"=====================");
+            msg.AppendLine($"{lesson.NumPair}.{lesson.NumLesson}");
+            msg.AppendLine($"{GetShortDiscipline(lesson.SubjectDetails.SubjectName)}");
+            msg.AppendLine($"{GetPrepareStringTeacher(lesson.Identity.First().Name)}");
+            msg.AppendLine($"Каб: {lesson.Cabs.FirstOrDefault()?.Adress} • {lesson.EducationGroup.Name}");
         }
 
         return Task.FromResult(msg.ToString());
+    }
+
+    private string GetPrepareStringTeacher(string teacherName)
+    {
+        teacherName = teacherName.Replace("  ", string.Empty)
+            .Replace("  ", string.Empty);
+        
+        var arraysTeacherName = teacherName.Split(' ');
+
+        if (arraysTeacherName.Length == 3)
+            return $"{arraysTeacherName[0]} {arraysTeacherName[1].FirstOrDefault()}. {arraysTeacherName[2].FirstOrDefault()}.";
+
+        return teacherName;
+    }
+
+    private string GetShortDiscipline(string disciplineName)
+    {
+        var arraysDisciplineName = disciplineName.Split(' ');
+
+        if (arraysDisciplineName.Length <= 3)
+            return disciplineName;
+
+        return $"{arraysDisciplineName[0]} {arraysDisciplineName[1]} {arraysDisciplineName[2]}...";
     }
 }
